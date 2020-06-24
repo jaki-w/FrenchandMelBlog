ec446b7b77d1bb7ec357c87eb04d1c0e9511a587
Jaki Widby <jaki.widby@gmail.com>
2020-06-24 13:50:09 -0700

    add js button functionality to add dog responses to DOM after removing previously printed responses.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,32 @@
 $(document).ready(function() {
-  $()
-})
+  $("button#hi").click(function() {
+    $('ul#Meli').children('li').remove();
+    $('ul#Frenchie').children('li').remove();
+    $('ul#Meli').prepend("<li>Hi, hi, hello bestie!</li>");
+    $('ul#Frenchie').prepend("<li>...hello, stranger.</li>");
+  });
+  $("button#bark").click(function() {
+    $('ul#Meli').children('li').remove();
+    $('ul#Frenchie').children('li').remove();
+    $('ul#Meli').prepend("<li>Woof! Woof, woof, woof! I love you so much!");
+    $('ul#Frenchie').prepend("<li>*awkward silence*</li>")
+  });
+  $("button#treat").click(function() {
+    $('ul#Meli').children('li').remove();
+    $('ul#Frenchie').children('li').remove();
+    $('ul#Meli').prepend("<li>Cheese, please!</li>");
+    $('ul#Frenchie').prepend("<li>The filthiest street food I can find. *sniff, sniff* Whatcha got for me?</li>");
+  });
+  $("button#trick").click(function() {
+    $('ul#Meli').children('li').remove();
+    $('ul#Frenchie').children('li').remove();
+    $('ul#Meli').prepend("<li>I only know one, but I will do it over and over if it will please you. *makes puppy eyes*</li>");
+    $('ul#Frenchie').prepend("<li>Maybe...first, show me the treat.</li>");
+  });
+  $("button#bye").click(function() {
+    $('ul#Meli').children('li').remove();
+    $('ul#Frenchie').children('li').remove();
+    $('ul#Meli').prepend("<li>Aww, five more minutes? I hate goodbyes!</li>");
+      $('ul#Frenchie').prepend("<li>*barely looks your way with indifference*</li>");
+  });
+});
