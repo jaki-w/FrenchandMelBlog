3c4fd3cb3f8752903cd455befeb6f15ba72cf31b
Jaki Widby <jaki.widby@gmail.com>
2020-06-25 15:35:44 -0700

    create a submit function with variables to hold input values.

---
+++ js/scripts2.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#formWalk").submit(function(event) {
+    const personInput = $("input#person").val();
+    const dogInput = $("input#dog").val();
+    const buildingInput = $("input#building").val();
+    const nounInput = $("input#noun").val();
+    const adjectiveInput = $("input#adjective").val();
+    const verbInput = $("input#verb").val();
+    const foodInput = $("input#food").val();
+  })
+})
