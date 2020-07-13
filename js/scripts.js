057e0889733578acfe57ea668ca698dc0a330f91
Jaki Widby <jaki.widby@gmail.com>
2020-07-13 12:15:27 -0700

    parseInt all values in quizSubmit function

--- js/scripts.js
+++ js/scripts.js
@@ -97,19 +97,19 @@ $(document).ready(function() {
 
 $(document).ready(function() {
   $("#quizSubmit").click(function(event) {
-     const q1 = $("input:radio[name=Q1]:checked").val();
-     const q2 = $("input:radio[name=Q2]:checked").val();
-     const q3 = $("input:radio[name=Q3]:checked").val();
-     const q4 = $("input:radio[name=Q4]:checked").val();
-     const q5 = $("input:radio[name=Q5]:checked").val();
-     const q6 = $("input:radio[name=Q6]:checked").val();
-     const q7 = $("input:radio[name=Q7]:checked").val();
-     const q8 = $("input:radio[name=Q8]:checked").val();
-     const q9 = $("input:radio[name=Q9]:checked").val();
-     const q10 = $("input:radio[name=Q10]:checked").val();
+     const q1 = parseInt($("input:radio[name=Q1]:checked").val());
+     const q2 = parseInt($("input:radio[name=Q2]:checked").val());
+     const q3 = parseInt($("input:radio[name=Q3]:checked").val());
+     const q4 = parseInt($("input:radio[name=Q4]:checked").val());
+     const q5 = parseInt($("input:radio[name=Q5]:checked").val());
+     const q6 = parseInt($("input:radio[name=Q6]:checked").val());
+     const q7 = parseInt($("input:radio[name=Q7]:checked").val());
+     const q8 = parseInt($("input:radio[name=Q8]:checked").val());
+     const q9 = parseInt($("input:radio[name=Q9]:checked").val());
+     const q10 = parseInt($("input:radio[name=Q10]:checked").val());
 
      let quizTotal;
-     
+
 
 
   });
